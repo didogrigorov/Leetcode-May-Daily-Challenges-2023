9042a0eea37f64a0366b2fd470b59014c6d3573d
Dilyan Grigorov <dido.grigorov@gmail.com>
2023-05-03 20:59:58 +0300

    Create Leetcode May Daily Challenge 2.py

---
+++ Leetcode May Daily Challenge 2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        p=nums[0]
+        for i in range(1,len(nums)):
+            p*=nums[i]
+        if p==0: 
+          return 0
+        elif p>0: 
+          return 1
+        else: return -1
